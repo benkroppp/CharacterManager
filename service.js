c322b1a01a99fff2db116944cf217067e28ec56e
benkroppp <ben.kropp@students.snow.edu>
2024-04-16 10:38:07 -0600

    trouble with converting to object

---
+++ service.js
@@ -0,0 +1,12 @@
+export const SaveCharacter = async (object) => {
+    const jsonchar = JSON.stringify(object);
+    await localStorage.setItem("character", jsonchar);
+}
+export const LoadCharacter = async () => {
+    const localchar = await localStorage.getItem("character");
+    console.log(localchar);
+    return JSON.parse(localchar);
+}
+export const DeleteData = () => {
+    localStorage.DeleteData();
+}
--- ui.js
+++ ui.js
@@ -1,7 +1,22 @@
 //imports
-
+import { SaveCharacter, LoadCharacter, DeleteData } from "./service.js";
 //element selectors
-const form = document.getElementById("form");
+const managerpage = document.getElementById("managerpage") !== null;
+if(!managerpage){
+const form = document.getElementById("creator-form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    const data = new FormData(e.target)
+    const values = Object.fromEntries(data.entries());
+    SaveCharacter(values);
 })
+}
+if(managerpage){
+    const deletechar = document.getElementById("delete");
+    deletechar.addEventListener("click", (e) => {
+        e.preventDefault();
+        DeleteData();
+    })
+}
+console.log(managerpage);
+LoadCharacter();
